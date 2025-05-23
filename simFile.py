1f548c4c02303583d74147734d372abd2a1154cf
Mason Rumuly <mrumuly@arista.com>
2025-05-23 00:17:50 -0500

    add YAML dump and load functions

---
+++ simFile.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2025 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+
+from __future__ import absolute_import, division, print_function
+from yaml import safe_load, safe_dump
+from sim import Topology
+
+def loadTopology( dump ):
+   state = safe_load( dump )
+   top = Topology()
+   for k, b in state[ 'behaviors' ].items():
+      top.addBehavior( k, b )
+   for k, n in state[ 'nodes' ].items():
+      top.addNode( k, **n )
+   for k, l in state[ 'links' ].items():
+      src, dst = eval( k )
+      if dst not in top.links.get( src, {} ):
+         top.addLink( src, dst )
+      link = top.links[ src ][ dst ]
+      link.maxDepth = l[ 'maxDepth' ]
+      link.queue = l[ 'queue' ]
+   return top
+
+def dumpTopology( topo ):
+   state = topo.dump()
+
+   # get block-style for the multiline code strings for easier editing
+   bKey = 'behaviors'
+   behaviors = { bKey: state[ bKey ] }
+   del state[ bKey ]
+   
+   return safe_dump( behaviors, default_style="|" ) + safe_dump( state )
+
+   print( dump )
+   loadTopology( dump )
+
